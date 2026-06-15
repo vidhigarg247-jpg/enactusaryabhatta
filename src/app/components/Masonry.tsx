569f03208b6ff11a1b83fa4ddf19da9d41ce4a4a
Tanmay <tanmay@Tanmays-MacBook-Pro.local>
2026-06-15 23:26:47 +0530

    fix: masonry default 2 columns for Android

--- src/app/components/Masonry.tsx
+++ src/app/components/Masonry.tsx
@@ -97,7 +97,7 @@ const Masonry: React.FC<MasonryProps> = ({
   const columns = useMedia(
     ['(min-width:1500px)', '(min-width:1000px)', '(min-width:600px)', '(min-width:400px)'],
     [5, 4, 3, 2],
-    1
+    2
   );
 
   const isMobile = columns <= 2;
